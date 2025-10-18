3fdbef8032a4e1024b1f6423bc957561c2580cba
William M. Tsikata <tsikatawill@gmail.com>
2025-10-18 20:08:44 +0000

    integrate assets

--- config/initializers/plutonium.rb
+++ config/initializers/plutonium.rb
@@ -3,5 +3,7 @@
 Plutonium.configure do |config|
   config.load_defaults 1.0
 
+  config.assets.stylesheet = "application"
+  config.assets.script = "application"
   # Configure plutonium above.
 end
